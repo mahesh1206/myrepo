3049d56d7c5d45f70914df07cf1bf97d1fe14a32
mahesh1206 <122433245+mahesh1206@users.noreply.github.com>
2024-07-21 15:09:07 +0530

    Add files via upload

---
+++ jubliantWebhookCurl.py
@@ -0,0 +1,7 @@
+curl --request POST \
+--url https://jfl-fxapi.fx-prod-apps.jublfood.com/api/CandidateOnboard/employeeOnboard \
+--header 'Authorization: Basic aWRmeTp2WlNjQjJhQUl4azZoMExvTHFXOTB6dDhUYg==' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+<FormatedResponse>
+}'
